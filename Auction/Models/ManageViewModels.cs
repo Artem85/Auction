8c2e831863c0948965ec262683c512daa2c72595
Zainchkovskyy Artem <zainchkovskyy.artem@gmail.com>
2020-02-18 08:51:34 +0200

    Revert unnecessary changes

--- Auction/Models/ManageViewModels.cs
+++ Auction/Models/ManageViewModels.cs
@@ -11,7 +11,7 @@ namespace Auction.Models
         public IList<UserLoginInfo> Logins { get; set; }
         public string PhoneNumber { get; set; }
         public bool TwoFactor { get; set; }
-        public bool BrowserRemembered { get; set; } 
+        public bool BrowserRemembered { get; set; }
     }
 
     public class ManageLoginsViewModel
